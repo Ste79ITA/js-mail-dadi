1965e397b1eb2a2509b917d293b0db6170518b74
Ste79ITA <nastri.s@gmail.com>
2023-10-09 16:29:57 +0200

    New Feature: dice

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
+// **************
 // MAIL
+// **************
 
 // Creazione array contente indirizzi mail.
 
@@ -15,19 +17,46 @@ const emailDatabase = [
 
 // - Chiedi all’utente la sua email.
 
-let userMail = prompt('Inserisci la tua mail');
-console.log(userMail);
+// let userMail = prompt('Inserisci la tua mail');
+// console.log(userMail);
 
 // - Controlla che sia nella lista di chi può accedere.
 
-let mailCheck = emailDatabase.includes(userMail);
+// let mailCheck = emailDatabase.includes(userMail);
 
-// - Stampa un messaggio appropriato sull’esito del controllo.
+// // - Stampa un messaggio appropriato sull’esito del controllo.
 
-if (mailCheck == true) {
-  console.log(`Utente riconosciuto, bentornato ${userMail}!`);
-} else {
-  console.log(
-    `L'indirizzo mail: ${userMail} non è corretto o inesistente. Accesso Negato`
-  );
-}
+// if (mailCheck == true) {
+//   console.log(`Utente riconosciuto, bentornato ${userMail}!`);
+// } else {
+//   console.log(
+//     `L'indirizzo mail: ${userMail} non è corretto o inesistente. Accesso Negato`
+//   );
+// }
+
+// **************
+// DADI
+// **************
+
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+const resultsDOMElement = document.querySelector('.results');
+const buttonDOMElement = document.querySelector('.play-btn');
+
+buttonDOMElement.addEventListener('click', function () {
+  let userDice = Math.floor(Math.random() * 6) + 1;
+  let computerDice = Math.floor(Math.random() * 6) + 1;
+
+  resultsDOMElement.innerHTML = `User: ${userDice}  |     Computer ${computerDice}`;
+
+  let message = '';
+
+  if (userDice > computerDice) {
+    message = 'HAI VINTO!';
+  } else if (userDice < computerDice) {
+    message = 'HAI PERSO!';
+  } else if (userDice == computerDice) {
+    message = 'PAREGGIO!';
+  }
+
+  resultsDOMElement.innerHTML += `<div class='message'>${message}</div>`;
+});
